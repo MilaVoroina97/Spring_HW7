78d6121f8ae9509f55746a69e81bb46433dabf27
mila <milavoronina1997@mail.ru>
2024-02-29 17:02:43 +0500

    finished all tasks

---
+++ src/main/java/ru/voronina/gb/hm7/SpringHW7Application.java
@@ -0,0 +1,13 @@
+package ru.voronina.gb.hm7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHW7Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringHW7Application.class,args);
+    }
+}
---
+++ src/test/java/ru/voronina/gb/hm7/SpringHW7ApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.voronina.gb.hm7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringHW7ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
